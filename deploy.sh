c04090b5e84c729b37ff67654aedfa782c2cdd9a
unknown <etbcor@gmail.com>
2023-08-17 14:11:42 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
-docker build -t etbcor/portfolio-site:latest .
-docker push etbcor/portfolio-site:latest
+docker build -t etbcor/portfolio-site:latest . && \
+docker push etbcor/portfolio-site:latest && \
 fly deploy
+
